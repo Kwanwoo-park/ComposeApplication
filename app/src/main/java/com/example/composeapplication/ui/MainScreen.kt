87351495dbd49c834ab807d195c0dbfc77e3e68c
Kwanwoo-park <65990897+Kwanwoo-park@users.noreply.github.com>
2022-12-12 22:18:14 +0900

    MainScreen update

--- app/src/main/java/com/example/composeapplication/ui/MainScreen.kt
+++ app/src/main/java/com/example/composeapplication/ui/MainScreen.kt
@@ -1,5 +1,7 @@
 package com.example.composeapplication.ui
 
+import android.content.Context
+import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -7,19 +9,24 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.example.composeapplication.BackHandler
+import androidx.navigation.NavDestination
+import androidx.navigation.compose.rememberNavController
 import com.example.composeapplication.navigation.NAV_ROUTE
 import com.example.composeapplication.NavButton
 import com.example.composeapplication.navigation.RouteAction
-import kotlinx.coroutines.coroutineScope
 
 @Composable
 fun MainScreen(routeAction: RouteAction) {
-    BackHandler(enable = true)  {}
-
     Surface(modifier = Modifier.fillMaxSize()) {
         Column(Modifier.padding(16.dp)) {
             NavButton(route = NAV_ROUTE.SETTING, routeAction = routeAction)
+//            Log.d("pkw", "MainScreen: ${Screen()}")
         }
     }
 }
+
+//@Composable
+//fun Screen(): Context{
+//    val nav = rememberNavController().context
+//    return nav
+//}
