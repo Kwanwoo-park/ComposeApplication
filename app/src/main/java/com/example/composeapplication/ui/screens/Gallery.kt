cc55907ab45cb70e782a5c09c98be7e065615ec1
Kwanwoo-park <akakslslzz@naver.com>
2023-03-30 20:00:46 +0900

    Image upload function & screen develop

--- app/src/main/java/com/example/composeapplication/ui/screens/Gallery.kt
+++ app/src/main/java/com/example/composeapplication/ui/screens/Gallery.kt
@@ -2,8 +2,12 @@ package com.example.composeapplication.ui.screens
 
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import androidx.activity.result.contract.ActivityResultContracts
 import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
@@ -18,13 +22,12 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import androidx.core.net.toUri
 import androidx.navigation.NavController
 import com.example.composeapplication.R
 import com.example.composeapplication.getNumberImage
@@ -47,7 +50,8 @@ fun Gallery(num: String, navController: NavController) {
     val context = LocalContext.current
     
     var comment by remember { mutableStateOf("") }
-    var photoUri by remember { mutableStateOf<Uri>("https://firebasestorage.googleapis.com/v0/b/composeapplication.appspot.com/o/images%2Fic_account.png?alt=media&token=bb6400d7-f24e-4cd8-8a1c-68a43c327fc8".toUri()) }
+    var photoUri by remember { mutableStateOf<Uri?>(null) }
+    var bitmap by remember { mutableStateOf<Bitmap?>(null) }
 
     val launcher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.StartActivityForResult(),
@@ -55,6 +59,16 @@ fun Gallery(num: String, navController: NavController) {
             if (results.resultCode == -1) {
                 photoUri = results.data?.data!!
                 Log.d("pkw", "Gallery: $photoUri")
+
+                bitmap = photoUri.let {
+                    if (Build.VERSION.SDK_INT < 28) {
+                        MediaStore.Images.Media.getBitmap(context.contentResolver, it)
+                    }
+                    else {
+                        val source = ImageDecoder.createSource(context.contentResolver, it!!)
+                        ImageDecoder.decodeBitmap(source)
+                    }
+                }
             }
         }
     )
@@ -70,19 +84,21 @@ fun Gallery(num: String, navController: NavController) {
             .fillMaxSize()
             .background(color = colorResource(id = R.color.baseBackground))) {
         Row {
-            Image(
-                painter = painterResource(id = R.drawable.ic_account),
-                contentDescription = "image",
-                modifier = Modifier
-                    .width(100.dp)
-                    .height(100.dp)
-                    .padding(8.dp)
-                    .clickable {
-                        val photoIntent = Intent(Intent.ACTION_PICK)
-                        photoIntent.type = "image/*"
-                        launcher.launch(photoIntent)
-                    }
-            )
+            if (photoUri != null){
+                Image(
+                    bitmap = bitmap!!.asImageBitmap(),
+                    contentDescription = "image",
+                    modifier = Modifier
+                        .width(100.dp)
+                        .height(100.dp)
+                        .padding(8.dp)
+                        .clickable {
+                            val photoIntent = Intent(Intent.ACTION_PICK)
+                            photoIntent.type = "image/*"
+                            launcher.launch(photoIntent)
+                        }
+                )
+            }
            Column(horizontalAlignment = Alignment.End){
                TextField(
                    value = comment,
@@ -101,7 +117,7 @@ fun Gallery(num: String, navController: NavController) {
                        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
                        val imageFileName = "JPEG_" + timeStamp + "_.png"
                        val storageRef = storage.reference.child("images").child(imageFileName)
-                       storageRef.putFile(photoUri).addOnSuccessListener {
+                       storageRef.putFile(photoUri!!).addOnSuccessListener {
                            Toast.makeText(context, R.string.upload_success, Toast.LENGTH_SHORT).show()
 
                            val contentDTO = ContentDTO()
